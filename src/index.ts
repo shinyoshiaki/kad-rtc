5427a53f6e625676467a1001c26dc8da81cb0a92
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-05-03 22:37:39 +0900

    export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import Kademlia from "./kademlia";
 import { PeerModule } from "./kademlia/modules/peer/webrtc";
 import GuestNode from "./node/guest";
 import PortalNode from "./node/portal";
+import { KvsModule } from "./kademlia/modules/kvs/base";
 
-export { PeerModule, Kademlia, GuestNode, PortalNode };
+export { PeerModule, Kademlia, GuestNode, PortalNode, KvsModule };
