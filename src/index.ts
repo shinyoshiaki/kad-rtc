8555c292bcd09be8037030a750d148dba9d149c3
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-05-03 22:01:26 +0900

    portalを分離

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import Kademlia from "./kademlia";
 import { PeerModule } from "./kademlia/modules/peer/webrtc";
 import GuestNode from "./node/guest";
-import PortalNode from "./node/portal";
 
-export { PeerModule, Kademlia, GuestNode, PortalNode };
+export { PeerModule, Kademlia, GuestNode };
