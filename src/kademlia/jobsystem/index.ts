bf4d81c4c09115640b41eec2d9684a1a9f3b38dc
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-05-15 16:00:26 +0900

    need e2e test

---
+++ src/kademlia/jobsystem/index.ts
@@ -0,0 +1,62 @@
+import Event from "rx.mini";
+
+type Option = { a: number };
+
+type AA<T> = T extends (...arg: infer I) => any ? I : never;
+type ThenArg<T> = T extends Promise<infer U> ? U : T;
+
+type Job = { func: any; args: any[]; event: Event<any> };
+
+class Worker {
+  running = false;
+
+  constructor(private jobs: Job[]) {}
+
+  private async execute() {
+    const job = this.jobs.shift();
+    if (job) {
+      this.running = true;
+
+      const { func, args, event } = job;
+      const res = await func(...args);
+      event.excute(res);
+
+      this.execute();
+    } else {
+      this.running = false;
+    }
+  }
+
+  wakeup() {
+    if (!this.running) {
+      this.execute();
+    }
+  }
+}
+
+export default class JobSystem {
+  jobs: Job[] = [];
+  workers: Worker[] = [];
+
+  constructor(private opt: Option = { a: 5 }) {
+    const { a } = opt;
+    this.workers = [...Array(a)].map(() => new Worker(this.jobs));
+  }
+
+  add<T extends (...args: any[]) => Promise<any>>(
+    func: T,
+    args: AA<typeof func>
+  ) {
+    const { a } = this.opt;
+
+    const event = new Event<ThenArg<ReturnType<T>>>();
+
+    this.jobs.push({ func, args, event });
+
+    if (this.jobs.length < a) {
+      this.workers.forEach(worker => worker.wakeup());
+    }
+
+    return event;
+  }
+}
---
+++ src/kademlia/jobsystem/test.ts
@@ -0,0 +1,40 @@
+import JobSystem from ".";
+
+const string = async (s: string, timeout?: number) => {
+  await new Promise(r => setTimeout(r, timeout));
+  return s;
+};
+
+describe("jobsystem", () => {
+  //   test(
+  //     "unit",
+  //     async () => {
+  //       const jobsys = new JobSystem({ a: 3 });
+  //       const ev = jobsys.add(string, ["s"]);
+  //       const res = await ev.asPromise();
+  //       expect(res).toBe("s");
+  //       expect(jobsys.jobs.length).toBe(0);
+  //     },
+  //     1000 * 60
+  //   );
+
+  test(
+    "multi",
+    async () => {
+      const jobsys = new JobSystem({ a: 3 });
+      const ev1 = jobsys.add(string, ["1", 1]);
+      const ev2 = jobsys.add(string, ["2", 1]);
+      const ev3 = jobsys.add(string, ["3", 1]);
+      const ev4 = jobsys.add(string, ["4", 2]);
+      expect(jobsys.jobs.length).toBe(1);
+
+      const d = await ev3.asPromise();
+      expect(d).toBe("3");
+      expect(jobsys.jobs.length).toBe(0);
+
+      const res = await ev4.asPromise();
+      expect(res).toBe("4");
+    },
+    1000 * 60
+  );
+});
