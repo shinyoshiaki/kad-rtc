52bca095da195caec546c0348893840796eed73c
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-05-18 01:01:02 +0900

    rm lib

--- lib/kademlia/di.d.ts
+++
@@ -1,7 +0,0 @@
-import Ktable from "./ktable";
-import Modules from "./modules";
-export declare type DependencyInjection = {
-    kTable: Ktable;
-    modules: Modules;
-};
-export declare const dependencyInjection: (kid: string, modules: Modules, opt?: Partial<import("./ktable/kbucket").Option>) => DependencyInjection;
--- lib/webrtc/utill/media.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function getLocalVideo(option?: MediaTrackSettings): Promise<MediaStream>;
-export declare function getLocalAudio(): Promise<MediaStream>;
-export declare function getLocalDesktop(option?: MediaTrackSettings): Promise<MediaStream>;
