c35a80fef6712d21673b591ca9b15d5f14100d5f
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-09-30 15:41:51 +0900

    eventmanager add selectable listen

--- src/kademlia/services/eventmanager/index.ts
+++ src/kademlia/services/eventmanager/index.ts
@@ -1,14 +1,15 @@
+import { Peer, RPC } from "../../modules/peer/base";
 import RpcManager, { ID } from "../rpcmanager";
 
 import Event from "rx.mini";
 import { FindNode } from "../../actions/findnode";
 import { FindValue } from "../../actions/findvalue";
-import { Peer } from "../../modules/peer/base";
 import { Store } from "../../actions/store";
 
 type Execute<T> = { res: T; peer: Peer };
 
 export default class EventManager {
+  event = new Event<RPC>();
   store = new Event<Execute<Store & ID>>();
   findnode = new Event<Execute<FindNode & ID>>();
   findvalue = new Event<Execute<FindValue & ID>>();
@@ -19,6 +20,11 @@ export default class EventManager {
     this.listenStore(peer);
     this.listenFindnode(peer);
     this.listenFindvalue(peer);
+
+    {
+      const { unSubscribe } = peer.onRpc.subscribe(this.event.execute);
+      peer.onDisconnect.once(unSubscribe);
+    }
   }
 
   private listenStore(peer: Peer) {
@@ -37,5 +43,16 @@ export default class EventManager {
     this.rpcManager
       .asObservable<FindValue>("FindValue", peer)
       .subscribe(res => this.findvalue.execute({ res, peer }));
+  }
+
+  selectListen<T extends RPC>(listenRPC: T) {
+    const event = new Event<T>();
+    this.event.subscribe(data => {
+      const { rpc } = data;
+      if (listenRPC.rpc === rpc) {
+        event.execute(data as T);
+      }
+    });
+    return event;
   }
 }
