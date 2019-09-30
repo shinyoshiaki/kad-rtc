ea0505338082f7fd32c4eedf82b4b2b885ece1a4
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-09-30 15:01:02 +0900

    event return peer

--- src/kademlia/services/eventmanager/index.ts
+++ src/kademlia/services/eventmanager/index.ts
@@ -6,10 +6,12 @@ import { FindValue } from "../../actions/findvalue";
 import { Peer } from "../../modules/peer/base";
 import { Store } from "../../actions/store";
 
+type Execute<T> = { res: T; peer: Peer };
+
 export default class EventManager {
-  store = new Event<Store & ID>();
-  findnode = new Event<FindNode & ID>();
-  findvalue = new Event<FindValue & ID>();
+  store = new Event<Execute<Store & ID>>();
+  findnode = new Event<Execute<FindNode & ID>>();
+  findvalue = new Event<Execute<FindValue & ID>>();
 
   constructor(public rpcManager: RpcManager) {}
 
@@ -22,18 +24,18 @@ export default class EventManager {
   private listenStore(peer: Peer) {
     this.rpcManager
       .asObservable<Store>("Store", peer)
-      .subscribe(this.store.execute);
+      .subscribe(res => this.store.execute({ res, peer }));
   }
 
   private listenFindnode(peer: Peer) {
     this.rpcManager
       .asObservable<FindNode>("FindNode", peer)
-      .subscribe(this.findnode.execute);
+      .subscribe(res => this.findnode.execute({ res, peer }));
   }
 
   private listenFindvalue(peer: Peer) {
     this.rpcManager
       .asObservable<FindValue>("FindValue", peer)
-      .subscribe(this.findvalue.execute);
+      .subscribe(res => this.findvalue.execute({ res, peer }));
   }
 }
