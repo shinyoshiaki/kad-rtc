c7cf9939850a2bd6491125089c2968ee0c8802fc
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-10-07 17:50:54 +0900

    export peermock

--- src/kademlia/index.ts
+++ src/kademlia/index.ts
@@ -1,12 +1,12 @@
 import { DependencyInjection, dependencyInjection } from "./di";
 import KeyValueStore, { Item } from "./modules/kvs/base";
 import { Peer, PeerMock } from "./modules/peer/base";
+import PeerModule, { PeerMockModule } from "./modules/peer";
 
 import EventManager from "./services/eventmanager";
 import Kademlia from "./kademlia";
 import Kbucket from "./ktable/kbucket";
 import Ktable from "./ktable";
-import PeerModule from "./modules/peer";
 import Uuid from "./util/uuid";
 import findNode from "./actions/findnode";
 import findValue from "./actions/findvalue";
@@ -32,7 +32,8 @@ export {
   Ktable,
   PeerMock,
   Uuid,
-  PeerModule
+  PeerModule,
+  PeerMockModule
 };
 
 export default Kademlia;
--- src/kademlia/modules/peer/index.ts
+++ src/kademlia/modules/peer/index.ts
@@ -1,3 +1,8 @@
+import { PeerMock } from "../..";
 import { PeerModule } from "./webrtc";
+
+const PeerMockModule = (kid: string) => new PeerMock(kid);
+
+export { PeerMockModule };
 
 export default PeerModule;
