74f1e8faff0331f04096b2e41cfb8353591b87df
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-10-08 17:24:31 +0900

    update test

--- src/tests/setupnetwork.test.ts
+++ src/tests/setupnetwork.test.ts
@@ -1,13 +1,23 @@
-import { PeerMockModule } from "../kademlia";
+import { PeerMockModule, PeerModule } from "../kademlia";
+
 import { testSetupNodes } from "./setupnetwork";
 
 describe("setup network", () => {
   test("mock", async () => {
-    // timeout at least 100
-    const nodes = await testSetupNodes(20, PeerMockModule, {
+    // 500msでタイムアウト発生
+    // 1sでタイムアウト無発生
+    const nodes = await testSetupNodes(10, PeerMockModule, {
       kBucketSize: 8,
-      timeout: 200
+      timeout: 1_000 / 2
     });
-    expect(nodes.length).toBe(20);
+    expect(nodes.length).toBe(10);
   }, 60_000_0);
+
+  // test("webrtc", async () => {
+  //   const nodes = await testSetupNodes(10, PeerModule, {
+  //     kBucketSize: 8,
+  //     timeout: 10_000
+  //   });
+  //   expect(nodes.length).toBe(10);
+  // }, 60_000_0);
 });
