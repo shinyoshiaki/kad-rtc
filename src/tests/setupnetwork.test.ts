2774e12ed49aa044eb23d1c81b20d13b5c47cd8e
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-10-09 11:12:54 +0900

    update test

--- src/tests/setupnetwork.test.ts
+++ src/tests/setupnetwork.test.ts
@@ -13,11 +13,11 @@ describe("setup network", () => {
     expect(nodes.length).toBe(10);
   }, 60_000_0);
 
-  // test("webrtc", async () => {
-  //   const nodes = await testSetupNodes(10, PeerModule, {
-  //     kBucketSize: 8,
-  //     timeout: 10_000
-  //   });
-  //   expect(nodes.length).toBe(10);
-  // }, 60_000_0);
+  test("webrtc", async () => {
+    const nodes = await testSetupNodes(10, PeerModule, {
+      kBucketSize: 8,
+      timeout: 1_000
+    });
+    expect(nodes.length).toBe(10);
+  }, 60_000_0);
 });
