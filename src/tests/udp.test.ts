792446f90a25ba41fae6e8ea4d9c5814109a9330
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-01-22 17:38:19 +0900

    fix

--- src/tests/udp.test.ts
+++ src/tests/udp.test.ts
@@ -14,37 +14,7 @@ describe("udp", () => {
       const a = PeerUdpModule("a");
       const b = PeerUdpModule("b");
 
-        const uuid = new Uuid();
-
-        const a = PeerModule("a");
-        const b = PeerModule("b");
-        const offer = await a.createOffer();
-        const answer = await b.setOffer(offer);
-        a.setAnswer(answer);
-
-        a.onConnect.once(async () => {
-          const data = { type: "a", msg: "a", id: uuid.get() };
-          a.rpc(data);
-          a.onRpc.once(v => {
-            const { msg } = v as any;
-            if (v.type === "b") {
-              expect(msg).toBe("b");
-              count.check();
-            }
-          });
-        });
-        b.onConnect.once(async () => {
-          const data = { type: "b", msg: "b", id: uuid.get() };
-          b.rpc(data);
-          b.onRpc.once(v => {
-            const { msg } = v as any;
-            if (v.type === "a") {
-              expect(msg).toBe("a");
-              count.check();
-            }
-          });
-        });
-      });
+      await testPeerModule(a, b);
       await closeUdpSocket();
     },
     1000 * 6000
