9185cb91c1daff98f0472cdb3d76e04a97bc5b00
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-01-22 17:32:33 +0900

    rename

--- src/kademlia/services/eventmanager/index.ts
+++ src/kademlia/services/eventmanager/index.ts
@@ -2,10 +2,10 @@ import { Peer, RPC } from "../../modules/peer/base";
 
 import Event from "rx.mini";
 
-type WithPeer<T> = { rpc: T; peer: Peer };
+type WithPeer = { rpc: RPC; peer: Peer };
 
 export default class EventManager {
-  event = new Event<WithPeer<RPC>>();
+  event = new Event<WithPeer>();
 
   constructor() {}
 
@@ -16,11 +16,11 @@ export default class EventManager {
     peer.onDisconnect.once(unSubscribe);
   }
 
-  selectListen<T extends RPC>(rpcCode: T["type"]) {
-    const event = new Event<WithPeer<T>>();
+  selectListen<T extends any>(rpcCode: keyof T) {
+    const event = new Event<WithPeer>();
     this.event.subscribe(({ rpc, peer }) => {
       if (rpcCode === rpc.type) {
-        event.execute({ rpc: rpc as T, peer });
+        event.execute({ rpc, peer });
       }
     });
     return event;
